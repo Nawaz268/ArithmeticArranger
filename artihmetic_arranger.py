9cf6cbc5bf9803864e00fb628355325aa53a18a5
Nawaz Hussain <nawaz26893@gmail.com>
2020-12-29 11:33:58 -0400

    InitialCommit

---
+++ artihmetic_arranger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 22:20:49 2020
+
+@author: nawaz
+"""
+start =2
+end =5
+
+t = "Hello world how are you"
+
+print(t[start:end])
+print("Hello" in t)
+t.sort()
